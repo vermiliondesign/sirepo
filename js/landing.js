2aaa1dafb077a3f1c25a5aa0e9f0d6c9eb63f441
Ian Zhang <ian@dxdy.io>
2019-05-07 22:46:27 -0600

    Make nav animation

--- js/landing.js
+++ js/landing.js
@@ -1,9 +1,15 @@
 $(function() {
   $('.landing .supported-codes').on('click', function() {
-    $('.landing .nav-takeover').removeClass('hidden');
+    $('.landing .nav-takeover').slideDown();
   });
 
   $('.landing .nav-takeover-close').on('click', function() {
-    $('.landing .nav-takeover').addClass('hidden');
+    $('.landing .nav-takeover').slideUp();
+  });
+
+  $(document).on('keyup', function(e) {
+    if (e.which == 27) {
+      $('.landing .nav-takeover').slideUp();
+    }
   });
 });
